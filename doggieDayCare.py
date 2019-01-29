4cb7a45d9ddd6ad322e0903ec9b27beafadb4e5a
vallettafox <43784504+vallettafox@users.noreply.github.com>
2019-01-29 13:16:35 -0500

    First Commit

---
+++ doggieDayCare.py
@@ -0,0 +1,14 @@
+doggieDayCare=['Skye','Mabel','Lassie','Buttercup']
+
+print(doggieDayCare)
+
+doggieDayCare + ["widget","Balto","Chloe"]
+
+doggieDayCare.sort()
+
+doggieDayCare[0:2]
+
+doggieDayCare.delete('Mabel')
+doggieDayCare.delete('Buttercup')
+
+print(doggieDayCare)
